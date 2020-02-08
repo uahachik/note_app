2bc0fd35e349e61291de5e8095998a561201fd98
kotenkoroman2019 <kotenkoroman2019@gmail.com>
2020-02-08 17:29:37 +0200

    pull request

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const Note = lazy(() => import('./components/notes/Note'));
 const ShiftNote = lazy(() => import('./components/notes/ShiftNote'));
 
 // pull-request prepare
-console.log('push request')
+console.log('pull request')
 
 const App = () => {
   const [provider, setProvider] = useState('fire_store');
